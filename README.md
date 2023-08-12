54af06b69715272c22552992e8d7c6cbdff0e8f3
ghubisz <gab.op.pl@op.pl>
2023-08-12 15:17:25 +0200

    done

---
+++ README.md
@@ -0,0 +1,7 @@
+Program written in python to track ethereum balance and transactions on any Ethereum account, it also tracks balance over time and then makes a graph of that using the Etherscan API.
+
+To turn it on (Linux):
+
+use the command: git clone with link of the repo
+then: python3 main.py
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from requests import get
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 API_KEY = "PXP99MX7J2WFNXWWUGSKADRVTIHAQ1F1I9"
 BASE_URL = "https://api.etherscan.io/api"
@@ -16,29 +17,49 @@ def make_api_url(module, action, address, **kwargs):
 
 def get_account_balance(address):
 
-    get_balance_url = make_api_url("account", "balance", address, tag="latest", x="2")
-    response = get(get_balance_url)
+    balance_url = make_api_url("account", "balance", address, tag="latest", x="2")
+    response = get(balance_url)
     data = response.json()
 
     value = (int(data["result"]) / ETHER_VALUE)
     return value
 
 def get_transactions(address):
-    get_transactions_url = make_api_url("account", "txlist", address, startblock =0, endblock=99999999, page=1, offset = 10000, sort="asc")
-    response = get(get_transactions_url)
+    transactions_url = make_api_url("account", "txlist", address, startblock =0, endblock=99999999, page=1, offset = 10000, sort="asc")
+    response = get(transactions_url)
     data = response.json()["result"]
+
+    internal_tx_url = make_api_url("account", "txlistinternal", address, startblock =0, endblock=99999999, page=1, offset = 10000, sort="asc")
+    response2 = get(internal_tx_url)
+    data2 = response2.json()["result"]
+
+    data.extend(data2)
+    data.sort(key= lambda x: int(x['timeStamp']))
+
+    current_balance = 0
+    balances = []
+    times = []
     
     for tx in data:
         to = tx["to"]
         from_addr = tx["from"]
-        value = tx["value"]
-        gas = tx["gasUsed"]
-        time = tx["timeStamp"]
-        print("------------------")
-        print("To:", to)
-        print("From:", from_addr)
-        print("Value:", value)
-        print("Gas Used:", gas)
-        print("Time:", time)
+        value = int(tx["value"]) / ETHER_VALUE
+        if "gasPrice" in tx:
+            gas = int(tx["gasUsed"]) * int(tx["gasPrice"]) / ETHER_VALUE
+        else:
+            gas = int(tx["gasUsed"]) / ETHER_VALUE
+        time = datetime.fromtimestamp(int(tx["timeStamp"]))
+        money_in = to.lower() == address.lower()
+
+        if money_in:
+            current_balance += value
+        else:
+            current_balance -= value + gas
+
+        balances.append(current_balance)
+        times.append(time)
+
+    plt.plot(times, balances)
+    plt.show()
 
 get_transactions(address)
